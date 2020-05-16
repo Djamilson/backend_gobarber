2088020fa38c99bd2d43f551528302171035d642
Djamilson <djamilson@gmail.com>
2020-05-16 12:42:53 -0300

    Update port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app';
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3333, () => {
   // const ip = require('ip');
   // console.log(ip.address());
    console.log('Started at http://localhost:%d');
